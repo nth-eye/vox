50b64cd963c69bae95b02e98d01111c5c4179494
Ilya Makarov <ilya.makarov.592@gmail.com>
2021-12-12 18:24:19 +0200

    comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# VOX-c
+# vox
+
+Dialogic ADPCM codec in C.
+
+## How to use
+
+```c
+VOX vox;
+
+uint8_t encoded[512]; // must be at least sizeof(samples) / 2
+int16_t samples[1024];
+
+// ...fill samples...
+
+vox_init(&vox);
+vox_encode(&vox, samples, encoded, sizeof(samples));
+
+```
+
+Example with reading from file can be found in main.c
+
+## TODO
+
+**_Nothing_**
--- inc/vox.h
+++ inc/vox.h
@@ -13,15 +13,57 @@ typedef struct {
     int16_t last;
 } VOX;
 
+/**
+ * @brief Reset codec state to default.
+ * 
+ * @param state Codec context
+ */
 void vox_init(VOX *state);
+
+/**
+ * @brief Encode 16-bit PCM samples to Dialogic ADPCM. Codec has 
+ * 4x compression, so each 16-bit sample will be encoded to nibble
+ * (4 bits). Two consecutive samples will be encoded as byte with 
+ * first sample being most significant nibble. 
+ * 
+ * @param state Codec context
+ * @param in Input samples
+ * @param out Encoded 
+ * @param len Input length in samples, not in bytes
+ */
 void vox_encode(VOX *state, int16_t *in, uint8_t *out, size_t len);
+
+/**
+ * @brief Decode dialogic ADPCM to 16-bit raw PCM. Each byte has 2 
+ * encoded samples and will be decoded as two int16_t's.
+ * 
+ * @param state Codec context
+ * @param in Encoded input bytes
+ * @param out Decoded output sample
+ * @param len Input length in bytes, not in samples
+ */
 void vox_decode(VOX *state, uint8_t *in, int16_t *out, size_t len);
 
+/**
+ * @brief Single encoding step.
+ * 
+ * @param state Codec context
+ * @param sample Sample to encode
+ * @return Encoded nibble (4 bits)
+ */
 uint8_t vox_encode_step(VOX *state, int16_t sample);
+
+/**
+ * @brief Single decoding step.
+ * 
+ * @param state Codec context
+ * @param code Encoded byte, contains 2 samples
+ * @return Decoded PCM sample
+ */
 int16_t vox_decode_step(VOX *state, uint8_t code);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // VOX_H
+#endif
